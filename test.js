a2ffdf0e09d1a9b3963a993f48ceddb1b3aef5ed
Andrew Bythell <abythell@ieee.org>
2018-07-03 17:03:30 -0700

    add new tests

--- test.js
+++ test.js
@@ -4,28 +4,33 @@ const expect = chai.expect
 const Joi = require('joi')
 const postalJoi = Joi.extend(require('./index.js'))
 
-describe('postalJoi', () => {
-  it('defaults to ISO country code US', () => {
-    let result = postalJoi.string().postalCode().validate('90210')
-    expect(result.error).to.equal(null)
-    expect(result.value).to.equal('90210')
-  })
-  it('accepts valid postal codes', () => {
+describe('joi-postalcode', () => {
+  it('returns value for valid postal codes', () => {
     let result = postalJoi.string().postalCode('CA').validate('A1A 1A1')
     expect(result.error).to.equal(null)
     expect(result.value).to.equal('A1A 1A1')
   })
-  it('rejects invalid postal codes', () => {
+  it('returns error for invalid postal codes', () => {
     let result = postalJoi.string().postalCode('CA').validate('A1A 111')
     expect(result.error).to.be.instanceof(Error)
     expect(result.error.message).to.be.a('string')
   })
-  it('validates all examples given in the postal-codes-js README', () => {
+  it('validates all values from the postal-codes-js README', () => {
     expect(postalJoi.string().postalCode('bg').validate('1003').error).to.equal(null)
     expect(postalJoi.string().postalCode('gb').validate('EC1A 1BB').error).to.equal(null)
     expect(postalJoi.string().postalCode('gb').validate('EC1A1BB').error).to.equal(null)
     expect(postalJoi.string().postalCode('gb').validate('EC1A-1BB').error).to.equal(null)
     expect(postalJoi.string().postalCode('tr').validate('33150').error).to.equal(null)
     expect(postalJoi.string().postalCode('us').validate('22313').error).to.equal(null)
+  })
+  it('throws if country code is not 2-letter ISO', () => {
+    expect(() => {
+      postalJoi.string().postalCode('Canada').validate('A1A 1A1')
+    }).to.throw('ISO 3166-1')
+  })
+  it('defaults to ISO country code US', () => {
+    let result = postalJoi.string().postalCode().validate('90210')
+    expect(result.error).to.equal(null)
+    expect(result.value).to.equal('90210')
   })
 })
