302a92571c24bd46972ec7503ef5e71cb5c8df15
Andrew Bythell <abythell@ieee.org>
2018-07-03 17:02:23 -0700

    comments and cleanup

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const postalCodes = require('postal-codes-js')
 
+/**
+ * Joi extension for validating postal codes.
+ * @see https://www.npmjs.com/package/postal-codes-js
+ * @see https://en.wikipedia.org/wiki/List_of_postal_codes
+ * @example
+ * const joiPostalCode = Joi.extend(require('joi-postalcode'))
+ * joiPostalCode.string().postalCode('CA').validate('A1A 1A1')
+ * joiPostalCode.string().postalCode('bg').validate('1003')
+ * joiPostalCode.string().postalCode('us').validate('22313')
+ */
 module.exports = joi => ({
   base: joi.string(),
   name: 'string',
@@ -9,7 +19,11 @@ module.exports = joi => ({
   rules: [{
     name: 'postalCode',
     params: {
-      cc: joi.string().length(2)
+      cc: joi.string().length(2).error(() => {
+        return {
+          message: 'parameter should be a 2-letter ISO 3166-1 country code'
+        }
+      })
     },
     validate (params, value, state, options) {
       if (!params.cc) params.cc = 'us'
